8029cfdd766bd8c74acce9e60552102d581d6dec
tyflo29 <tyflo29400@gmail.com>
2020-12-09 12:40:28 +0100

    v1

--- Florian_Breton-Code_Python.py
+++ Florian_Breton-Code_Python.py
@@ -6,31 +6,30 @@ from random import randrange
 
 mot1='parfum'
 motDonner='bacfgd'
-motReserve=''
-motReserve1=''
+motReserve=[]
+motReserve1=[]
 motFinal = ''
 print(mot1)
 print(motDonner)
+lettrebonne=''
+malposition=''
+pasdedans=''
+
 
 for i in range(0,6):
-    motReserve += str(ord(motDonner[i]))
-    motReserve += ','
-    
-    motReserve1 += str(ord(mot1[i]))
-    motReserve1 += ','
-
-for i in range(0,6):   
-    a = motReserve[i]
-    b = motReserve1[i]
-    if (a == b):
-        motFinal += str(Back.RED + motDonner[i] + Style.RESET_ALL)
-    elif a in motReserve1:
-        motFinal += str(Back.YELLOW + motDonner[i] + Style.RESET_ALL)
+    if (ord(mot1[i]) == ord(motDonner[i])):
+        lettrebonne += str(Back.RED + motDonner[i] + Style.RESET_ALL)
+        
+    elif ord(motDonner[i]) in motReserve1:
+        malposition += str(Back.YELLOW + motDonner[i] + Style.RESET_ALL)
+        
     else:
-        motFinal += str(Back.BLUE + motDonner[i] + Style.RESET_ALL)
-    
-print(motReserve)
+        pasdedans += str(Back.BLUE + motDonner[i] + Style.RESET_ALL)
+
+print("Vous avait les lettres",lettrebonne,"qui est bon")
+print("Vous avait les lettres",malposition,"qui est pas a la bonne place") 
+print("Vous avait les lettres",pasdedans,"qui est pas dedans")  
+
 print(motReserve1)
 print(mot1)
 print(motDonner)
-print(motFinal)
