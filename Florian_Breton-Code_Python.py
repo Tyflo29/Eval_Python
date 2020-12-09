9d454cafe591aba8a0bd4ffdfe4d1110d97e9a5b
tyflo29 <tyflo29400@gmail.com>
2020-12-09 12:16:24 +0100

    Update Florian_Breton-Code_Python.py

--- Florian_Breton-Code_Python.py
+++ Florian_Breton-Code_Python.py
@@ -1,32 +1,36 @@
 from colorama import init 
-init() 
+init(wrap = False) 
 from colorama import Fore, Back, Style 
 from random import randrange
 
-nombreChoisie=randrange(3)
-listeDeMots=['crepis','crabe','parfum']
-mot1= listeDeMots[nombreChoisie]
-motDonner=''
-motDonner=input()
+
+mot1='parfum'
+motDonner='bacfgd'
 motReserve=''
 motReserve1=''
 motFinal = ''
 print(mot1)
+print(motDonner)
+
 for i in range(0,6):
     motReserve += str(ord(motDonner[i]))
     motReserve += ','
     
     motReserve1 += str(ord(mot1[i]))
     motReserve1 += ','
+
+for i in range(0,6):   
+    a = motReserve[i]
+    b = motReserve1[i]
+    if (a == b):
+        motFinal += str(Back.RED + motDonner[i] + Style.RESET_ALL)
+    elif a in motReserve1:
+        motFinal += str(Back.YELLOW + motDonner[i] + Style.RESET_ALL)
+    else:
+        motFinal += str(Back.BLUE + motDonner[i] + Style.RESET_ALL)
     
-    if (motReserve[i] == motReserve1[i]):
-        motFinal += str(Back.RED + motDonner[i])
-    if motDonner[i] in mot1:
-        motFinal += str(Back.YELLOW + motDonner[i])
-        print(Style.RESET_ALL)
-
-
 print(motReserve)
 print(motReserve1)
 print(mot1)
+print(motDonner)
 print(motFinal)
